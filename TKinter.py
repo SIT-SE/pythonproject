75276905530fe11ac802c1d8526cdd2fbb0a44c0
mayinot <maymadiaung99@gmail.com>
2020-10-08 13:24:46 +0800

    Search Function
    Completed search function abit flimsy but it works. If have time will improve on it.

--- TKinter.py
+++ TKinter.py
@@ -1,9 +1,11 @@
 # note program need install xlrd
-import checkdataframes
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
+import checkdataframes
+import searchcriterion
 import pandas as pd
 
+"""GUI code"""
 # initialised the tkinter GUI
 root = tk.Tk()
 
@@ -17,7 +19,7 @@ Tree_Frame.place(height=400, width=900)
 
 # Frame for open file dialog
 file_frame = tk.LabelFrame(root, text="Open File")
-file_frame.place(height=150, width=400, rely=0.65, relx=0)
+file_frame.place(height=100, width=300, rely=0.65, relx=0)
 
 # Buttons to read excel files
 browse_button = tk.Button(file_frame, text="Browse A File", command=lambda: File_dialog())
@@ -29,6 +31,11 @@ load_button.place(rely=0.65, relx=0.30)
 # The file/file path text
 Selected_FLabel = ttk.Label(file_frame, text="No File Selected")
 Selected_FLabel.place(rely=0, relx=0)
+
+# Search Criterion
+search_button = tk.Button(root, text="Search Data", command=lambda: search_cri())
+search_button.pack(side=tk.BOTTOM)
+
 
 # Tree view widget in my 1st frame
 Tree_View1 = ttk.Treeview(Tree_Frame)
@@ -43,12 +50,67 @@ TVScroll_x.pack(side='bottom', fill='x')
 TVScroll_y.pack(side='right', fill='y')
 
 
+def search_cri():
+    file_path = Selected_FLabel["text"]
+    column_names = checkdataframes.get_columns(file_path)
+    search_data = tk.Tk()
+    search_data.title("Search data")
+    search_data.geometry("800x600")  # set the root dimensions
+
+
+
+    def search_now():
+        selected = drop.get()
+        data_frame = checkdataframes.checkfiletype(file_path)
+        searched = search_box.get()
+        searched_df = data_frame[data_frame[selected].str.contains(searched)]
+        print(searched_df)
+        Tree_View2["column"] = list(searched_df.columns)
+        # Show only the headings
+        Tree_View2["show"] = "headings"
+        for column in Tree_View2["column"]:
+            Tree_View2.heading(column, text=column)  # let the column heading = column name
+        # turns the dataframe into a list of lists
+        searched_df_rows = searched_df.to_numpy().tolist()
+        # inserts each list into the treeview
+        for row in searched_df_rows:
+            Tree_View2.insert("", "end", values=row)
+        return None
+
+    # Entry box to search data
+    search_box = tk.Entry(search_data)
+    search_box.grid(row=0, column=1, padx=10, pady=10)
+    # Search box label search for customer
+    search_box_label = tk.Label(search_data, text="Search Data: ")
+    search_box_label.grid(row=0, column=0, padx=10, pady=10)
+    # Entry box search Button customer
+    search_button = tk.Button(search_data, text="Search", command=search_now)  # add command
+    search_button.grid(row=1, column=0, padx=10)
+
+    # Drop down Box
+    drop = ttk.Combobox(search_data, values=column_names)
+    drop.current(0)
+    drop.grid(row=0, column=2)
+
+    # Frame for TreeView Data search
+    Tree2_Frame = tk.LabelFrame(search_data, text="Search Data")
+    Tree2_Frame.grid(row=3, column=0)
+
+    # Tree view 2 widget in my 1st frame
+    Tree_View2 = ttk.Treeview(Tree2_Frame)
+    Tree_View2.grid(row=3, column=0)
+
+
+
+"""Functions for uploading files"""
+
+
 def File_dialog():
     """This Function will open the file explorer and assign the chosen file path to label_file"""
     filename = filedialog.askopenfilename(initialdir="/",
                                           title="Select A File",
                                           filetype=(("xlsx files", "*.xlsx"), ("text files", "*.txt"),
-                                                    ("csv files", "*.csv"),("json files", "*.json"),
+                                                    ("csv files", "*.csv"), ("json files", "*.json"),
                                                     ("All Files", "*.*")))
     Selected_FLabel["text"] = filename
     return None
